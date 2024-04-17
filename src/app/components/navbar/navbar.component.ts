e68851a349dc967e1754ed14aa872d96c8988aca
kwabenakay <67112259+kwabenakay@users.noreply.github.com>
2024-04-17 09:01:37 +0000

    feat(navigation): toogle sidebar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { LogoComponent } from '../partial/logo/logo.component';
 import { ButtonComponent } from '../partial/button/button.component';
+import { BoardDataService } from '../../service/board-data/board-data.service';
+import { Destroyer } from '../../utils/destroyer.util';
 
 @Component({
   selector: 'app-navbar',
@@ -9,4 +11,18 @@ import { ButtonComponent } from '../partial/button/button.component';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent extends Destroyer implements OnInit, OnDestroy {
+  private boardService = inject(BoardDataService);
+  public isSidebarOn = true;
+  ngOnInit(): void {
+    this.addSub(
+      this.boardService.getIsSidebarOn().subscribe((val) => {
+        this.isSidebarOn = val;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.destroySubs()
+  }
+}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { BoardDataService } from '../../service/board-data/board-data.service';
+import { CommonModule } from '@angular/common';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
+  private boardService = inject(BoardDataService);
+  public boards = this.boardService.getAvailableBoards();
+  public sidebarState = true;
+  public isSidebarOn = this.boardService.getIsSidebarOn().pipe(
+    tap((val) => {
+      this.sidebarState = val;
+    })
+  );
+  public toggleSidebar(): void {
+    this.boardService.emitIsSidebar(!this.sidebarState)
+  }
 }
